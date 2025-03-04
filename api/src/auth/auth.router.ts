a688d72010d7c886234763aa7d4229599c74c7c4
MohakGupta2004 <mohakgupta500@gmail.com>
2025-03-05 04:48:17 +0530

    feat: zod validation add at auth router

--- api/src/auth/auth.router.ts
+++ api/src/auth/auth.router.ts
@@ -1,7 +1,14 @@
-import { Router } from "express";
-
+import express, { Router } from "express";
+import { signupSchema } from "../schemas/signUpSchema";
 const authRouter = Router()
 
+authRouter
+  .post('/register', (req: express.Request, res: express.Response)=>{
+    const {success, data, error} = signupSchema.safeParse(req.body);
+    if(!success) res.send(400).send(error.issues)
+    
+    
+})
 
 
 export default authRouter
